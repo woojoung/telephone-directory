a33ace43dadcb32d0d43f615fc0c50a9ff5d798c
woojoung <chae3229@gmail.com>
2023-10-18 16:52:26 +0900

    feat: 사용하지 않는 메소드 제거

--- src/MenuViewer.java
+++ src/MenuViewer.java
@@ -5,18 +5,14 @@ public class MenuViewer {
     private static Scanner scan = new Scanner(System.in);
     public MenuViewer() {}
 
-    public static Scanner inputKeyboard() {
-        return scan;
-    }
-
     // 번호 선택
-    public static int selectMenu() {
+    public int selectMenu() {
         System.out.print(" Enter The Menu >>> ");
         return scan.nextInt();
     }
 
     // 메뉴 보여주기
-    public static void showMenu() {
+    public void showMenu() {
         System.out.println(" [ADD: 1, DELETE: 2, SEARCH: 3, DISPLAY_LIST: 4, EXIT: 0]");
     }
 }
