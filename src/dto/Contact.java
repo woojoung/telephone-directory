074442f8d9659bfb4ef774009661061fd695f588
Woojoung <chae3229@gmail.com>
2023-11-05 19:57:48 +0900

    refactor: contact 클래스 dto 하위 이동

--- src/dto/Contact.java
+++ src/dto/Contact.java
@@ -1,15 +1,17 @@
+package dto;
+
 import java.io.Serializable;
 
 public class Contact implements Serializable {
     // 연락처 저장 기본 정보, 인스턴스 변수 (non-static)
     String name, phoneNumber;
 
-    // Contact 클래스 생성자
+    // dto.Contact 클래스 생성자
     public Contact(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
-    // Contact 클래스 메소드
+    // dto.Contact 클래스 메소드
     public String getName() {
         return name;
     }
